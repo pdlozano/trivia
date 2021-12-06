11e210c7239e9784cc9e8e66ddcdaa7320d3abb2
David Lozano <hello@davidlozano.me>
2021-12-06 11:25:04 +0800

    Fix Categories Problem

--- modules/questions.ts
+++ modules/questions.ts
@@ -1,29 +1,4 @@
-const enum Category {
-    GeneralKnowledge = 9,
-    Books = 10,
-    Film = 11,
-    Music = 12,
-    MusicalsAndTheatre = 13,
-    Television = 14,
-    VideoGames = 15,
-    BoardGames = 16,
-    ScienceAndNature = 17,
-    Computers = 18,
-    Mathematics = 19,
-    Mythology = 20,
-    Sports = 21,
-    Geography = 22,
-    History = 23,
-    Politics = 24,
-    Art = 25,
-    Celebrities = 26,
-    Animals = 27,
-    Vehicles = 28,
-    Comics = 29,
-    Gadgets = 30,
-    AnimeAndManga = 31,
-    CartoonAndAnimations = 32,
-}
+import type { Category } from "./categories";
 
 const enum Difficulty {
     Easy = "easy",
@@ -85,5 +60,5 @@ class Questions {
     }
 }
 
-export { Questions, Category, Difficulty };
+export { Questions, Difficulty };
 export type { Question };
